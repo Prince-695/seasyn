84c431817ac761329cbce44a20a5e77e6dd59c38
Prince Rathod <rathodprince411@gmail.com>
2026-06-24 13:43:42 +0530

    frontend fixes

--- frontend/src/components/shared/Footer.tsx
+++ frontend/src/components/shared/Footer.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom"
 import { Mail } from "lucide-react"
-import { FaGithub } from "react-icons/fa"
+import { FaGithub, FaLinkedin } from "react-icons/fa"
 
 const footerColumns = [
   {
